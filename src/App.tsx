59c96dca39be90aff6a019928c0720b76688a088
valvalov22 <vlrik@mail.ru>
2023-03-12 15:51:30 +0400

    react lazy and other stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import './scss/app.scss';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
+
+const Cart = React.lazy(() => import('./pages/Cart'));
+const FullPizza = React.lazy(() => import('./pages/FullPizza'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/pizza/:id' element={<FullPizza />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/cart' element={<React.Suspense fallback={<div>Идет загрука корзины...</div>}> <Cart /> </React.Suspense>} />
+        <Route path='/pizza/:id' element={<React.Suspense fallback={<div>Идет загрука...</div>}> <FullPizza /> </React.Suspense>} />
+        <Route path='*' element={<React.Suspense fallback={<div>Идет загрука...</div>}> <NotFound /> </React.Suspense>} />
       </Route>
     </Routes>
   )
--- src/redux/slices/pizza/asyncActions.ts
+++ src/redux/slices/pizza/asyncActions.ts
@@ -8,7 +8,7 @@ export const fetchPizzas = createAsyncThunk<TPizza[], Record<string, string>>(
       const { sortBy, order, category, search, currentPage } = params;
       const { data } = await axios
       .get<TPizza[]>(
-        `https://63e4c1e08e1ed4ccf6e54873.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+        `https://63e4c1e08e1ed4ccf6e54873.mockapi.io/items?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`,
       )
       return data;
     }
